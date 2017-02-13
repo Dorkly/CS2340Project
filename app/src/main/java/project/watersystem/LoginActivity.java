29148a7f6c5d38c95b274db83f5738f7e1f4cb57
Dorkly <bree_cakesx3@live.com>
2017-02-13 10:52:39 -0500

    Welcome Screen rv3

--- app/src/main/java/project/watersystem/LoginActivity.java
+++ app/src/main/java/project/watersystem/LoginActivity.java
@@ -65,7 +65,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome_screen);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
