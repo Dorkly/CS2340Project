9a1f6c157fb56e78bc63e07bc8f6ee5115c573b3
hmguthrie <hmguthrie@live.com>
2017-02-18 18:49:24 -0500

    "Functionality for cancel profile button"

--- app/src/main/java/project/watersystem/ProfileActivity.java
+++ app/src/main/java/project/watersystem/ProfileActivity.java
@@ -1,13 +1,29 @@
 package project.watersystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+    public Button cancelProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        cancelProfileButton = (Button) findViewById(R.id.cancelProfile);
+        cancelProfileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent intent = new Intent(ProfileActivity.this, AppScreen.class);
+                startActivity(intent);
+                finish();
+                //setContentView(R.layout.activity_login);
+            }
+        });
     }
 }
