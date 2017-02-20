d93c9e046c9de6b2f81d8799555eedf36c8af648
AustinJ's Mac <AustinJ@lawn-128-61-31-16.lawn.gatech.edu>
2017-02-20 10:23:15 -0500

    add registration screen

---
+++ app/src/main/java/project/watersystem/RegistrationScreen.java
@@ -0,0 +1,46 @@
+package project.watersystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by AustinJ on 2/19/17.
+ */
+
+public class RegistrationScreen extends AppCompatActivity {
+    public Button cancelRegButton;
+    public Button saveRegButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+
+        /**cancelRegButton = (Button) findViewById(R.id.cancelProfile);
+        cancelRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent intent = new Intent(RegistrationScreen.this, WelcomeScreen.class);
+                startActivity(intent);
+                finish();
+                //setContentView(R.layout.activity_login);
+            }
+        });
+
+        saveRegButton = (Button) findViewById(R.id.save);
+        saveRegButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent intent = new Intent(RegistrationScreen.this, AppScreen.class);
+                startActivity(intent);
+                finish();
+                //setContentView(R.layout.activity_login);
+            }
+        });
+         */
+
+    }
+}
--- app/src/main/java/project/watersystem/WelcomeScreen.java
+++ app/src/main/java/project/watersystem/WelcomeScreen.java
@@ -1,11 +1,10 @@
 package project.watersystem;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 /**
  * Welcome screen that allows user to login or register.
@@ -30,6 +29,19 @@ public class WelcomeScreen extends AppCompatActivity {
                 Intent intent = new Intent(WelcomeScreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
+                //setContentView(R.layout.activity_login);
+            }
+        });
+
+        regButton = (Button) findViewById(R.id.button2);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent intent2 = new Intent(WelcomeScreen.this, RegistrationScreen.class);
+                startActivity(intent2);
+                finish();
+
                 //setContentView(R.layout.activity_login);
             }
         });
