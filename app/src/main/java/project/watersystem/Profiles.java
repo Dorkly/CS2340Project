3adb4354c8625a6edf9c769a6412ee50c28264a5
Jim helm <jeh1746@gmail.com>
2017-02-22 13:05:59 -0500

    Added JavaDoc comments to Users, Profiles, and DatabaseHandler

--- app/src/main/java/project/watersystem/Profiles.java
+++ app/src/main/java/project/watersystem/Profiles.java
@@ -4,12 +4,21 @@ package project.watersystem;
  * Created by jimhelm on 2/22/17.
  */
 
+/**
+ * Profiles class used for Profile information
+ */
 public class Profiles {
 
     String _userId;
     String _homeAddress;
     String _phone;
 
+    /**
+     * Profiles constructor
+     * @param userId user ID
+     * @param _homeAddress user name
+     * @param _phone user password
+    */
     public Profiles(String userId, String _homeAddress, String _phone) {
         this._userId = userId;
         this._homeAddress = _homeAddress;
@@ -17,17 +26,26 @@ public class Profiles {
 
     }
 
-    // getting User
+    /**
+     * Get user id
+     * @return String
+     */
     public String getUser(){
         return this._userId;
     }
 
-    // getting Password
+    /**
+     * Get Home Address
+     * @return String
+     */
     public String getHomeAddress(){
         return this._homeAddress;
     }
 
-    // getting User
+    /**
+     * Get Phone Number
+     * @return String
+     */
     public String getPhone(){
         return this._phone;
     }
