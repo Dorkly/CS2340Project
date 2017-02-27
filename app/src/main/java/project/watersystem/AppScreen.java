4f62f76a37fae3b80796d53d158391e2693765cc
AustinJ's Mac <AustinJ@lawn-128-61-23-61.lawn.gatech.edu>
2017-02-27 10:30:51 -0500

    fix error and add main report screen

--- app/src/main/java/project/watersystem/AppScreen.java
+++ app/src/main/java/project/watersystem/AppScreen.java
@@ -1,11 +1,9 @@
 package project.watersystem;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -63,22 +61,7 @@ public class AppScreen extends AppCompatActivity {
                 //setContentView(R.layout.activity_login);
             }
         });
-<<<<<<< HEAD
 
-        //Main Report ;
-        reportButton = (Button) findViewById(R.id.Logout);
-        reportButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(AppScreen.this, MainReportScreen.class);
-                startActivity(intent);
-                finish();
-
-                //setContentView(R.layout.activity_welcome_screen);
-            }
-        });
-
-=======
->>>>>>> 57c539251cad1ac6de6d867ffee3fe7d7cf23561
     }
     public void report(){
         Intent intent = new Intent(AppScreen.this, Splash.class);
@@ -91,28 +74,7 @@ public class AppScreen extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-    @Override
-<<<<<<< HEAD
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-=======
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.miCompose:
-                report();
-                return true;
-            case R.id.miProfile:
-                profile();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
->>>>>>> 57c539251cad1ac6de6d867ffee3fe7d7cf23561
-    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
