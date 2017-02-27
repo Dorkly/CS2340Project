4a6b4cef6cc15cb4e6c5b69ccab71b6ab107fe6b
Jim helm <jeh1746@gmail.com>
2017-02-27 10:43:54 -0500

    Comment out menu

--- app/src/main/java/project/watersystem/AppScreen.java
+++ app/src/main/java/project/watersystem/AppScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -92,7 +93,7 @@ public class AppScreen extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 }
