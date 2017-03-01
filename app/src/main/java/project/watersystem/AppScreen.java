3860ce20b79637362190bb8a872f15627a05ac71
hmguthrie <hmguthrie@live.com>
2017-03-01 08:42:45 -0500

    App Screen Change

--- app/src/main/java/project/watersystem/AppScreen.java
+++ app/src/main/java/project/watersystem/AppScreen.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 /**
  * Main application screen after login.
