7a2f16d0c37d0f2ea41bbab10d9b3f6d28badd23
AustinJ's Mac <AustinJ@lawn-128-61-23-61.lawn.gatech.edu>
2017-03-05 15:58:52 -0500

    google Map

---
+++ app/src/main/java/project/watersystem/MapsActivity.java
@@ -0,0 +1,126 @@
+package project.watersystem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+/**
+ * Created by AustinJ on 3/5/17.
+ */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    //private ModelFacade mFacade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.activity_map);
+        mapFragment.getMapAsync(this);
+
+        //mFacade = ModelFacade.getInstance();
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Setting a click event handler for the map
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+
+
+                // Clears the previously touched position
+                // mMap.clear();
+                //mFacade.addReport("newly added", "Bobs Place", new Location(latLng.latitude, latLng.longitude));
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                //markerOptions.title(mFacade.getLastReport().getName());
+                //markerOptions.snippet(mFacade.getLastReport().getDescription());
+
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+            }
+        });
+        /*
+        List<Report> reportList = mFacade.getReports();
+        for (Report r : reportList) {
+            LatLng loc = new LatLng(r.getLatitude(), r.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(loc).title(r.getName()).snippet(r.getDescription()));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        }
+
+        mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter());
+        */
+    }
+    /*
+
+    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+
+        private final View myContentsView;
+
+        CustomInfoWindowAdapter(){
+            myContentsView = getLayoutInflater().inflate(R.layout.custom_info_contents, null);
+        }
+
+
+        @Override
+        public View getInfoContents(Marker marker) {
+
+            TextView tvTitle = ((TextView)myContentsView.findViewById(R.id.title));
+            tvTitle.setText(marker.getTitle());
+            //TextView tvSnippet = ((TextView)myContentsView.findViewById(R.id.snippet));
+            //tvSnippet.setText(marker.getSnippet());
+
+            return myContentsView;
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+
+    }
+    */
+
+}
+
