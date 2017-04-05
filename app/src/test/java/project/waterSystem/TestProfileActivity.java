61538fde00ddd8927d1e0ac995b075d8e89d3204
hmguthrie <hmguthrie@live.com>
2017-04-05 17:33:21 -0400

    Testing class for hannah

---
+++ app/src/test/java/project/waterSystem/TestProfileActivity.java
@@ -0,0 +1,30 @@
+package project.waterSystem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by hmgut_000 on 4/4/2017.
+ */
+
+public class TestProfileActivity {
+    /**
+     * The test fixture
+     */
+    private ProfileActivity profileActivity;
+    /**
+     * This method is run before each test
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+       profileActivity = new ProfileActivity();
+    }
+
+    @Test
+    public void testFindPosition() {
+
+        Assert.assertEquals();
+    }
+}
