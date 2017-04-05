7a1a45fade6041f5af98d74544178ad0710a8cf4
AustinJ's Mac <AustinJ@lawn-128-61-28-7.lawn.gatech.edu>
2017-04-05 16:22:29 -0400

    add MapsActivityTest

---
+++ app/src/test/java/project/waterSystem/MapsActivityTest.java
@@ -0,0 +1,13 @@
+package project.waterSystem;
+
+import project.waterSystem.Controller.MapsActivity;
+
+/**
+ * Created by AustinJ on 4/5/17.
+ */
+
+public class MapsActivityTest {
+    private MapsActivity testMapsAcvity;
+
+
+}
