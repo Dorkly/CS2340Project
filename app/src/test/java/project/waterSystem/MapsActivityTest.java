7486a0e6070dab779ecfa35468e9bc8c6a30ece4
AustinJ's Mac <AustinJ@lawn-128-61-28-7.lawn.gatech.edu>
2017-04-06 10:44:51 -0400

    resize image again

--- app/src/test/java/project/waterSystem/MapsActivityTest.java
+++ app/src/test/java/project/waterSystem/MapsActivityTest.java
@@ -1,13 +1,42 @@
 package project.waterSystem;
 
-import project.waterSystem.Controller.MapsActivity;
+import org.junit.Before;
+import org.junit.Test;
+
+import project.waterSystem.Controller.LoginActivity;
 
 /**
  * Created by AustinJ on 4/5/17.
  */
 
 public class MapsActivityTest {
-    private MapsActivity testMapsAcvity;
+    private LoginActivity testLoginActivity;
+
+    @Before
+    public void setUp() {
+        testLoginActivity = new LoginActivity();
+    }
+
+    @Test
+    public void testAdd() {
+        //assertEquals("Simple Add result incorrect",56, testLoginActivity.execute(16, 40, "+"));
+    }
+
+    @Test
+    public void testAttemptLogin() {
+        /*
+        Database db = mock(Database.class);
+        when(db.getStudentIDFor("Bob")).thenReturn("XY32F14");
+        when(db.getStudentNameFor(1234)).thenReturn("Bob Waters");
+
+        assertEquals("XY32F14", db.getStudentIDFor("Bob"));
+        assertNull(db.getStudentIDFor("Sally"));
+
+        assertEquals("Bob Waters", db.getStudentNameFor(1234));
+        assertNull(db.getStudentNameFor(5463));
+        */
+    }
+
 
 
 }
